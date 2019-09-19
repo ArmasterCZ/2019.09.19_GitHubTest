583e7e61ce9f0e86ea596ee370c30b02ad1bf428
Armaster <Akrijfuadlaf@seznam.cz>
2019-09-19 22:50:21 +0200

    added line master.

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Base program.");
+            Console.WriteLine("Base line.");
         }
     }
 }
