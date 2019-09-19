b9a42fb25e5d77e356e3f02eb90396fced14df63
Armaster <Akrijfuadlaf@seznam.cz>
2019-09-19 22:45:37 +0200

    added class and line.

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Base program.");
+            Console.WriteLine("Added in testing1");
         }
     }
 }
